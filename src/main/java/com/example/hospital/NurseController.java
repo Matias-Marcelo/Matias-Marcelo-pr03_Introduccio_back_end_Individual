8af4c2914826d77f405b7af31a12e438a18af16f
Matias <Matias@DESKTOP-1JTV287>
2024-10-08 17:49:02 +0200

    Added Comments

--- src/main/java/com/example/hospital/NurseController.java
+++ src/main/java/com/example/hospital/NurseController.java
@@ -14,25 +14,23 @@ public class NurseController {
 	 private ArrayList<Nurse> nurses;
 	 //ArrayList
 
-	   // Constructor que inicializa la lista de enfermeros
+	    // Constructor to initialize the list of nurses with data
 	    public NurseController() {
 	        nurses = new ArrayList<>();
 	        nurses.add(new Nurse("Alberto", "password1"));
 	        nurses.add(new Nurse("Maria", "password2"));
 	        nurses.add(new Nurse("Juan", "password3"));
 	    }
-//	
-//
+
+	    // Endpoint login 
 	    @PostMapping("/login")
 	    public ResponseEntity <Boolean> login(@RequestParam String user, @RequestParam String password) {
-
-	        for (Nurse nurse : nurses) {
-	            
-	            // Verificamos si el usuario y la contraseña coinciden
+	        for (Nurse nurse : nurses) { 
 	            if (nurse.user().equals(user) && nurse.password().equals(password)) {
-	                return ResponseEntity.ok(true); 
+	                return ResponseEntity.ok(true); // Return HTTP 200 (OK)
 	            }
 	        }
+	        // Return HTTP 401 (Error)
 	        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false); 
 
 	    }
